fafe30f8d554efc6d27e576b3b1aa6aaddede16c
KMurphy-5 <kevincitorios@gmail.com>
2021-02-20 13:36:36 -0800

    changes made to CustomerBL

---
+++ P0/StoreApp/StoreBL/CustomerBL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using StoreModels;
+using StoreDL;
+
+namespace StoreBL
+{
+    public class CustomerBL : IStoreBL
+    {
+        private IStoreRepository _repo;
+
+        public CustomerBL(IStoreRepository repo){
+            _repo = repo;
+        }
+        public void AddCustomer(Customer newCustomer)
+        {
+            _repo.AddCustomer(newCustomer);
+        }
+
+        public List<Customer> GetCustomer()
+        {
+            return _repo.GetCustomer();
+        }
+    }
+
+}
