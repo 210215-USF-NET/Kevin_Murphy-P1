962fd014409d68d783aa149faa43f372c69874b5
KMurphy-5 <kevincitorios@gmail.com>
2021-02-20 15:16:24 -0800

    trying to get file writing to work

---
+++ P0/StoreApp/StoreBL/PartsBL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using StoreModels;
+using StoreDL;
+
+namespace StoreBL
+{
+    public class PartsBL : IPartsBL
+    {
+        private IStoreRepository _repo;
+
+        public PartsBL(IStoreRepository repo){
+            _repo = repo;
+        }
+        public void AddCustomer(Customer newCustomer)
+        {
+            _repo.AddCustomer(newCustomer);
+        }
+
+        public List<Customer> GetCustomer()
+        {
+            return _repo.GetCustomer();
+        }
+    }
+
+}
